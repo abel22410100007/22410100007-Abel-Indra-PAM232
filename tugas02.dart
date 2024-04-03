173704e93cf9e9efe97b81f290b1c71a0ef07d10
abel22410100007 <164967132+abel22410100007@users.noreply.github.com>
2024-04-03 21:18:49 +0700

    Add files via upload

---
+++ tugas02.dart
@@ -0,0 +1,9 @@
+void main () {
+  for(int i = 0; i<=100; i++) {
+    if(i%2 == 0) {
+      print ("Bilangan Ganjil");
+    } else {
+      print ("Bilangan Genap");
+    }
+  }
+}
---
+++ tugas07.dart
@@ -0,0 +1,10 @@
+void main () {
+  int nilai = 80;
+  if (nilai > 70) {
+    print ('Baik');
+  } else if (nilai > 60 && nilai < 70) {
+    print ('Sedang');
+  } else {
+    print ('Kurang');
+  }
+}
